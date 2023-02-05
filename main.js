0faef1c7a57271099c17d19aa6647c359970968a
Isabela Espinoza <isabela.espinoza@correo.unimet.edu.ve>
2023-02-05 13:10:54 -0400

    About me
    Commit 2 actaulizando main

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var imagenes = ['x','https://mott.pe/noticias/wp-content/uploads/2019/12/C%C3%B3mo-es-el-uso-de-l%C3%ADneas-horizontales-en-la-composici%C3%B3n-de-fotograf%C3%ADa-1280x720.png','https://escueladeposgrado.edu.pe/wp-content/uploads/2021/12/Ingenieri%CC%81adesistemasenmexico.jpg','ima3.jpeg','ima4.jpeg'],
-=======
-var imagenes = ['x','https://img.interempresas.net/fotos/1795779.jpeg','https://escueladeposgrado.edu.pe/wp-content/uploads/2021/12/Ingenieri%CC%81adesistemasenmexico.jpg','ima3.jpeg','ima4.jpeg'],
->>>>>>> About_me
+var imagenes = ['ima4.jpeg','https://img.interempresas.net/fotos/1795779.jpeg','https://escueladeposgrado.edu.pe/wp-content/uploads/2021/12/Ingenieri%CC%81adesistemasenmexico.jpg','ima3.jpeg','ima4.jpeg'],
 cont = 0;
 
 function carrousel(contenedor){
